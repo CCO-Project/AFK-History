93678d0abf7cceb474eb8a1abe473764321998d8
LianSheng197 <ci.i.cxcvii@gmail.com>
2022-02-18 01:50:42 +0800

    init

---
+++ tools/AFK-collect.js
@@ -0,0 +1,184 @@
+/**
+ * @typedef Message
+ * @property {Array} attachments
+ * @property {Object} author
+ * @property {String} channel_id
+ * @property {Array} components
+ * @property {String} content
+ * @property {any} edited_timestamp
+ * @property {Array} embeds
+ * @property {Number} flags
+ * @property {Boolean} hit
+ * @property {String} id
+ * @property {Boolean} mention_everyone
+ * @property {Array} mention_roles
+ * @property {Array} mentions
+ * @property {Boolean} pinned
+ * @property {String} timestamp
+ * @property {Boolean} tts
+ * @property {Number} type
+ * @property {String} webhook_id
+ */
+
+/**
+ * @typedef OriginalResult
+ * @property {String} analytics_id
+ * @property {number} total_results
+ * @property {Array<Array<Message>>} messages
+ */
+
+/**
+ * @typedef Result
+ * @property {boolean} ok
+ * @property {OriginalResult} data
+ */
+
+/**
+ * 取得搜尋資料
+ * 
+ * @param {number} ts
+ * @param {number} offset 資料查詢偏移值
+ * @param {String} authorizationToken 授權驗證參數（用來判斷你的 Discord 帳號）
+ * @param {String} xSuperProperties 授權驗證參數（用來判斷你的 Discord 帳號，不過我不確定這個是不是每個帳號有唯一值，反正我是拆開）
+ * @returns {Promise<Result>}
+ */
+async function getData(ts, offset, authorizationToken, xSuperProperties) {
+    let min_id = getSnowflake(ts);
+    let max_id = getSnowflake(ts, true);
+    let content = "has used Global Time Skip, all online players currently performing AFK task";
+
+    let data = await fetch(`https://discord.com/api/v9/guilds/742992894167744552/messages/search?min_id=${min_id}&max_id=${max_id}&content=${encodeURI(content)}&sort_by=timestamp&sort_order=asc&offset=${offset}`, {
+        "headers": {
+            "accept": "*/*",
+            "accept-language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+            "authorization": authorizationToken,
+            "x-debug-options": "bugReporterEnabled",
+            "x-discord-locale": "zh-TW",
+            "x-super-properties": xSuperProperties
+        },
+        "referrer": "https://discord.com/channels/742992894167744552/742992894167744555",
+        "referrerPolicy": "strict-origin-when-cross-origin",
+        "body": null,
+        "method": "GET",
+        "mode": "cors",
+        "credentials": "include"
+    }).then(
+        r => r.json()
+    ).then(
+        r => ({
+            ok: true,
+            data: r
+        })
+    ).catch(
+        e => ({
+            ok: false,
+            data: e
+        })
+    );
+
+    return data;
+}
+
+/**
+ * 取得指定日期的雪花值 (用於 Discord 搜尋參數)
+ * 
+ * @param {number} ts timestamp
+ * @param {boolean} addOneDay
+ * @returns {number}
+ */
+function getSnowflake(ts, addOneDay = false) {
+    if (addOneDay) {
+        ts += 24 * 60 * 60 * 1000;
+    }
+
+    ts -= 1420070400000;
+    return Number(BigInt(ts) << BigInt(22))
+}
+
+/**
+ * @typedef DateObject
+ * @property {String} date
+ * @property {Number} ts timestamp
+ */
+
+/**
+ * 從開始日期往後偏移 offsetDays 天，回傳 timestamp
+ * 
+ * @param {number} startY
+ * @param {number} startM
+ * @param {number} startD
+ * @param {number} [offsetDays]
+ * @returns {DateObject}
+ */
+function getTimeStampWithOffset(startY, startM, startD, offsetDays = 0) {
+    let ts = new Date(`${startY}-${startM}-${startD}`).getTime();
+    ts += offsetDays * 24 * 60 * 60 * 1000;
+
+    let d = new Date(ts);
+    let yyyy = d.getFullYear();
+    let mm = `0${d.getMonth()+1}`.slice(-2);
+    let dd = `0${d.getDate()}`.slice(-2);
+
+    return {
+        date: `${yyyy}-${mm}-${dd}`,
+        ts: ts
+    };
+}
+
+/**
+ * 等待指定秒數
+ * 
+ * @param {number} second
+ */
+function waitSeconds(second = 6) {
+    return new Promise(resolve => setTimeout(resolve, second * 1000));
+}
+
+// ================ [ MAIN ] ================ //
+
+// 請先設定這兩個參數值，否則請求必定失敗。
+const authorizationToken = "";
+const xSuperProperties = "";
+// ----------------
+
+
+var allData = {};
+(async () => {
+    const startDate = [2021, 9, 17];
+    const endDate = [2022, 2, 5];
+
+    let start = getTimeStampWithOffset(...startDate, 0);
+    let end = getTimeStampWithOffset(...endDate);
+    let offsetDay = 0;
+    while (true) {
+        console.log(start.date, startDate, offsetDay);
+
+        if (start.ts > end.ts) {
+            break;
+        }
+
+        let total = 99999;
+        let temp = [];
+
+        for (let i = 0; i < total; i += 25) {
+            await waitSeconds();
+            let data = await getData(start.ts, i, authorizationToken, xSuperProperties);
+
+            if (data.ok) {
+                console.log(` - ${(i/25)+1}`);
+                total = data.data.total_results;
+                temp = temp.concat(data.data.messages.map(e => e[0]));
+            } else {
+                console.log(startDate, offsetDay, `offset(data)=${i}`, data.data);
+                throw new Error("請求錯誤");
+            }
+        }
+
+        allData[start.date] = temp;
+
+        offsetDay += 1;
+        start = getTimeStampWithOffset(...startDate, offsetDay);
+    }
+
+    console.log(allData);
+})();
---
+++ tools/AFK-parser.js
@@ -0,0 +1,86 @@
+const data1 = require("./AFK-collection-20210625-20210719.json");
+const data2 = require("./AFK-collection-20210720-20210916.json");
+const data3 = require("./AFK-collection-20210917-20220205.json");
+
+const fs = require("fs");
+
+/**
+ * @typedef MessageAuthor
+ * @property {Boolean} bot
+ * @property {String} id
+ * @property {String} username
+ * @property {*} avatar
+ * @property {String} discriminator
+ */
+
+/**
+ * @typedef Message
+ * @property {Array} attachments
+ * @property {MessageAuthor} author
+ * @property {String} channel_id
+ * @property {Array} components
+ * @property {String} content
+ * @property {*} edited_timestamp
+ * @property {Array} embeds
+ * @property {Number} flags
+ * @property {Boolean} hit
+ * @property {String} id
+ * @property {Boolean} mention_everyone
+ * @property {Array} mention_roles
+ * @property {Array} mentions
+ * @property {Boolean} pinned
+ * @property {String} timestamp
+ * @property {Boolean} tts
+ * @property {Number} type
+ * @property {String} webhook_id
+ */
+
+/**
+ * @typedef {Object.<string, Array<Message>>} MessageCollection
+ */
+
+/**
+ * @typedef SortedData
+ * @property {String} p [p]layerName
+ * @property {Number} c affect players [c]ount
+ * @property {Number} m skip [m]inutes
+ * @property {String} t [t]imestamp
+ */
+
+/**
+ * Parse data
+ * @param {MessageCollection} collection
+ * @returns {Array<SortedData>}
+ */
+function parser(collection) {
+    /** @type {Array<SortedData>} */
+    let temp = [];
+
+    Object.values(collection).forEach(messages => {
+        messages.forEach(msg => {
+            if(msg.author.bot === true) {
+                let matches = msg.content.match(/^([^\ ]+).+?\(([0-9]+)\ players\).+?skip\ by\ ([0-9]+)\ minutes/);
+
+                if(matches !== null) {
+                    temp.push({
+                        p: matches[1],
+                        c: matches[2] - 0,
+                        m: matches[3] - 0,
+                        t: msg.timestamp
+                    });
+                }
+            }
+        });
+    });
+
+    return temp;
+}
+
+let allData = [];
+
+allData = allData.concat(parser(data1));
+allData = allData.concat(parser(data2));
+allData = allData.concat(parser(data3));
+
+
+fs.writeFileSync("./result/all-data.json", JSON.stringify(allData, null, '\t'));
